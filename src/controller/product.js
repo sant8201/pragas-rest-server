46f7707021261cd2af01a0121433d19feb81a762
Jorge Luis dos Santos <71775223+sant8201@users.noreply.github.com>
2021-03-27 21:04:11 +0300

    Update product.js

--- src/controller/product.js
+++ src/controller/product.js
@@ -6,7 +6,7 @@ const Category = require("../models/category");
 exports.createProduct = (req, res) => {
   //res.status(200).json( { file: req.files, body: req.body } );
 
-  const { name, price, description, category, quantity, createdBy } = req.body;
+  const { name, price, description, productPictures, category, quantity, createdBy } = req.body;
   let productPictures = [];
 
   if (req.files.length > 0) {
